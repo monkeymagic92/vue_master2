cdc7e2edfdd270d7df3d59ddd75708a129f024b4
monkeymagic92 <ddw0099@naver.com>
2024-04-03 11:22:50 +0900

    T

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <div>
+      <h2>Home View</h2>
+      <p>{{ $route.path }}</p>
+      <button class="btn btn-primary" @click="goAboutPage">Home 으로 이동</button>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const goAboutPage = () => {
+  router.push('/about')
+}
+</script>
+
+<style lang="scss" scoped></style>
